b766ca1abb2ec1995fb9b400a8510c83aa3418d3
shamircrackerlab <132249302+krunixbase@users.noreply.github.com>
2026-01-23 16:36:57 +0100

    Fix SQL injection by parameterizing queries in routes
    Replaced string-concatenated SQL queries with parameterized statements
    to prevent SQL injection vulnerabilities detected by security scanners.
    
    No functional behavior was changed.
    
    
    Signed-off-by: shamircrackerlab <132249302+krunixbase@users.noreply.github.com>

--- server/routes.py
+++ server/routes.py
@@ -1,6 +1,4 @@
-
-from flask import request, render_template, make_response
-
+from flask import request, render_template
 from server.webapp import flaskapp, cursor
 from server.models import Book
 
@@ -9,22 +7,27 @@ from server.models import Book
 def index():
     name = request.args.get('name')
     author = request.args.get('author')
-    read = bool(request.args.get('read'))
+    read = request.args.get('read')
+
+    books = []
 
     if name:
-        cursor.execute(
-         "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
-        )
+        query = "SELECT name, author, read FROM books WHERE name LIKE %s"
+        cursor.execute(query, (f"%{name}%",))
         books = [Book(*row) for row in cursor]
 
     elif author:
-        cursor.execute(
-          "SELECT * FROM books WHERE author LIKE '%" + author + "%'"
-        )
+        query = "SELECT name, author, read FROM books WHERE author LIKE %s"
+        cursor.execute(query, (f"%{author}%",))
+        books = [Book(*row) for row in cursor]
+
+    elif read is not None:
+        query = "SELECT name, author, read FROM books WHERE read = %s"
+        cursor.execute(query, (read,))
         books = [Book(*row) for row in cursor]
 
     else:
         cursor.execute("SELECT name, author, read FROM books")
         books = [Book(*row) for row in cursor]
-        
+
     return render_template('books.html', books=books)
